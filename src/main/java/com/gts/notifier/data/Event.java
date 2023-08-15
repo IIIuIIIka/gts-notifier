301c9a50b551f9358f836d185b290d46a5c9a0cd
drakm <drakm@home01.mshome.net>
2023-08-15 22:49:52 +0300

    Event basics

--- src/main/java/com/gts/notifier/data/Event.java
+++ src/main/java/com/gts/notifier/data/Event.java
@@ -1,5 +1,33 @@
 package com.gts.notifier.data;
 
+import java.sql.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name="events")
 public class Event {
 
+	@Id
+	@Column(name="id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(name="message")
+	private String message;
+	
+	@Column(name="event_date_time")
+	private Date eventDateTime;
+		
 }
